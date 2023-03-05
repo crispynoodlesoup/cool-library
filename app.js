cdf9edefb7a15ed0d49615d8b2743702dca87906
crispynoodlesoup <thepanman321@gmail.com>
2023-03-04 21:32:43 -0600

    implement form requirements

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
+//modal
+const modal = document.querySelector(".modal");
+const modalContent = document.querySelector(".modal-content");
+
+//buttons
 const addBook = document.querySelector(".add-book");
 const submitBook = document.querySelector(".submit-book");
 const cancel = document.querySelector(".cancel");
-const modal = document.querySelector(".modal");
+
+//form inputs
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const pageCountInput = document.getElementById("page-count");
+const descriptionInput = document.getElementById("description");
+const readInput = document.getElementById("read");
 
 addBook.addEventListener("click", (e) => {
   modal.style.visibility = "visible";
   modal.style.display = "grid";
 });
 
-submitBook.addEventListener("click", (e) => {
+cancel.addEventListener("click", (e) => {
   modal.style.visibility = "hidden";
   modal.style.display = "none";
 });
 
-cancel.addEventListener("click", (e) => {
+modalContent.addEventListener("submit", (e) => {
+  //check title is not empty
+  e.preventDefault();
+
   modal.style.visibility = "hidden";
   modal.style.display = "none";
 });
